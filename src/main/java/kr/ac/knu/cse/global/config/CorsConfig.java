c774dfb7860c264ffb969da5f9c797e922ddf4a3
lsmin3388 <lsmin3388_@naver.com>
2025-02-26 11:20:27 +0900

    feat: Spring Security 설정

---
+++ src/main/java/kr/ac/knu/cse/global/config/CorsConfig.java
@@ -0,0 +1,39 @@
+package kr.ac.knu.cse.global.config;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class CorsConfig {
+
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(Arrays.asList(
+			"http://localhost:3000",
+			"http://localhost:5173",
+			"https://example.com"
+		));
+		configuration.setAllowedMethods(Arrays.asList(
+			HttpMethod.GET.name(),
+			HttpMethod.POST.name(),
+			HttpMethod.PATCH.name(),
+			HttpMethod.PUT.name(),
+			HttpMethod.DELETE.name()
+		));
+		configuration.setAllowCredentials(true);
+		configuration.setAllowedHeaders(List.of("*"));
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+
+		return source;
+	}
+}
