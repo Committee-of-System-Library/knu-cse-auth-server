748817bbecbbefdf58278b911cedadb8b828e39b
lsmin3388 <lsmin3388_@naver.com>
2025-03-15 22:22:37 +0900

    fix: V1__init.sql 코드 복원

--- src/main/resources/db/migration/V2__Add_RefreshToken_And_Provider_Tables.sql
+++ src/main/resources/db/migration/V2__Add_RefreshToken_And_Provider_Tables.sql
@@ -1,23 +1,21 @@
--- V2__Add_RefreshToken_And_Provider_Tables.sql
-
 CREATE TABLE refresh_token
 (
-    email         VARCHAR(255) NOT NULL,
-    refresh_token VARCHAR(255) NOT NULL,
-    created_at    TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at    TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    email         varchar(255) NOT NULL,
+    refresh_token varchar(255) NOT NULL,
+    created_at    timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at    timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (email)
 );
 
 CREATE TABLE provider
 (
     provider_id   bigint       NOT NULL AUTO_INCREMENT,
-    email         VARCHAR(255) NOT NULL,
-    provider_name VARCHAR(255) NOT NULL,
-    provider_key  VARCHAR(255) NOT NULL,
-    student_id    BIGINT       NULL,
-    created_at    TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at    TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    email         varchar(255) NOT NULL,
+    provider_name varchar(255) NOT NULL,
+    provider_key  varchar(255) NOT NULL,
+    student_id    bigint       NULL,
+    created_at    timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at    timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (provider_id),
     UNIQUE KEY provider_email_uq (email),
     CONSTRAINT provider_student_fk
