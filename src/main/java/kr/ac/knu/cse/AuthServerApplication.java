88ad28e20f42e6d275f96750f5e7392accd4f727
lsmin3388 <lsmin3388_@naver.com>
2025-09-14 15:59:40 +0900

    feat: Logback을 이용한 로깅 추가

--- src/main/java/kr/ac/knu/cse/AuthServerApplication.java
+++ src/main/java/kr/ac/knu/cse/AuthServerApplication.java
@@ -1,15 +1,18 @@
 package kr.ac.knu.cse;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @EnableCaching
+@Slf4j
 public class AuthServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AuthServerApplication.class, args);
+        log.info("Auth Server Application started successfully.");
     }
 
 }
